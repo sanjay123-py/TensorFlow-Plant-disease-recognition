2bdaedcff0c2d1fc6a10906f1602794c11a8ae4a
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-10-12 16:16:53 +0530

    Webapp Files

---
+++ Model.py
@@ -0,0 +1,88 @@
+import keras.models
+import tensorflow as tf
+import os
+import numpy as np
+class model:
+    def __init__(self,filename,selected_model):
+        self.filename=filename
+        self.selected_model=selected_model
+
+        self.labels={"Apple_model" : ['Apple_Scab disease',
+                              'Apple Black_rot',
+                              'Apple edar_apple_rust',
+                              'Apple healthy'],
+        "Corn_model" : ['Corn_(maize) Cercospora_leaf_spot Gray_leaf_spot',
+                             'Corn_(maize) Common_rust_',
+                             'Corn_(maize) Northern_Leaf_Blight',
+                             'Corn_(maize) healthy'],
+        "Peach_model" : ['Peach Bacterial_spot',
+                              'Peach healthy'],
+        "Tomato_model" : ['Tomato Bacterial_spot',
+                               'Tomato Early_blight',
+                               'Tomato Late_blight',
+                               'Tomat Leaf_Mold',
+                               'Tomato Septoria_leaf_spot',
+                               'Tomato Spider_mites Two-spotted_spider_mite',
+                               'Tomato Target_Spot',
+                               'Tomato Tomato_Yellow_Leaf_Curl_Virus',
+                               'Tomato Tomato_mosaic_virus',
+                               'Tomato healthy'],
+        "Potato_model" :['Potato Early_blight',
+                               'Potato Late_blight',
+                               'Potato healthy'],
+        "Grape_model" : ['Grape Black_rot',
+                              'Grape Esca_(Black_Measles)',
+                              'Grape Leaf_blight_(Isariopsis_Leaf_Spot)',
+                              'Grape healthy'],
+
+        "StrawBerry_model" : ['Strawberry Leaf_scorch',
+                                   'Strawberry healthy'],
+        "Cherry_model" : ['Cherry_(including_sour) Powdery_mildew',
+                               'Cherry_(including_sour) healthy']
+        }
+
+    """
+    Load and preprocess the data,the main duty of this function is to convert the images into 3 channeled
+    numerical data and rescale it by 1/255. and expands the dimension at axis 1
+    thats because of the batch_size is the first dimension 
+    structure is od dimesnion: (batch_size,width,height,channels)
+    """
+
+    def load_and_prep_data(self,path):
+        img = tf.io.read_file(path)
+        # decode the image into tensor
+        tensor = tf.io.decode_image(img, channels=3)
+
+        image = tf.image.resize(tensor, [256, 256])
+        # normalize the image
+        image = image / 255.
+
+        # we need to expand the first axis beacuse of batch size
+        image = tf.expand_dims(image, axis=0)
+        # (1,256,256,3)
+        return image
+
+    '''
+    Given the preprocessed image data and model for a specific class used to predict the patterns
+    wand gives the probability of each classes
+    '''
+
+    def predict_and_visualize(self):
+        pro_image = self.load_and_prep_data(self.filename)
+
+        #Selecting a random model form the selected plant model from [model2.h5,model3.h5]
+        #Because both the models performs good and so i decided to use bothnthe models
+
+        random_model=np.random.choice([x for x in os.listdir(f"Tensorflow_Plant_disease_recognition_model/{self.selected_model}") if x.endswith('.h5')])
+        model=keras.models.load_model (f"Tensorflow_Plant_disease_recognition_model/{self.selected_model}/{random_model}")
+        pred= model.predict(pro_image)
+
+        #In case of multi-class labels we use the condtion pred[0]>1
+        if(len(pred[0])>1):
+            index=tf.argmax(pred[0])
+            return self.labels[self.selected_model][index]
+        # if pred[0] == 1 then it is an binary classification problem
+        else:
+            index=int(np.round(pred[0][0]))
+            return self.labels[self.selected_model][index]
+
---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+import sys
+from flask import Flask,render_template,url_for, request,flash,redirect
+from Model import model
+
+#Create an app
+app=Flask(__name__)
+app.secret_key="sanjay"
+
+#Allowe file extensions
+ALLOWED_EXTENSION=['jpeg','jpg','png']
+
+
+#Home page
+@app.route("/",methods=['POST','GET'])
+def home():
+    return render_template("index.html")
+
+'''
+Funtion to check whether the extension of the uploaded file is in ALLOWED_EXTENSION list
+'''
+
+def check_file_name(filename:str):
+    if(filename.split(".")[1].lower() in ALLOWED_EXTENSION):
+        return True
+    return False
+
+'''
+Get and save the image and redirect the image for preprocessing and prediction of the image
+'''
+
+@app.route("/get_img",methods=["POST"])
+def get_img():
+    if request.method=='POST':
+        if("image" not in  request.files):
+            flash("No image is selected.",category="error")
+            return redirect(url_for("home"))
+        plant_image=request.files['image']
+
+        if(plant_image.filename==''):
+            flash("No image is selected to be uploaded",category="error")
+            return redirect(url_for("home"))
+
+        if(not check_file_name(plant_image.filename)):
+            flash("File with .jpeg, .jpg, .png is only accepted.",category="error")
+            return redirect(url_for("home"))
+        selected_model=request.form.get("sel_mod")
+        if(selected_model=="None"):
+            flash("No Model is selected.",category="error")
+            return redirect(url_for("home"))
+        else:
+            plant_image.save(os.path.join("static/uploads/",plant_image.filename))
+            return redirect(url_for("predict",image_name=plant_image.filename,selected_model=selected_model))
+
+# After redirecting from the get_img we predict the disease and that is done by model class from Model.py
+
+@app.route('/<image_name>/<selected_model>',methods=['POST','GET'])
+def predict(image_name,selected_model):
+    try:
+        image_path = os.path.join("static/uploads/",image_name)
+        model1=model(image_path,selected_model)
+        prediction=model1.predict_and_visualize()
+        return render_template("result.html",path=image_path,prediction=prediction)
+    except:
+        flash("Someting error happend",category="error")
+        return redirect(url_for("home"))
+if __name__ == '__main__':
+    app.run(port=4321)
